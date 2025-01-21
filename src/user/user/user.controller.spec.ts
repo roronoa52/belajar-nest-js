7825db40e3bcb66026cbdc83cccb40b051ab7249
roronoa52 <farhan.yudha2016we@gmail.com>
2025-01-21 16:11:10 +0700

    belajar mock dan unit testing

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMocks from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -15,4 +16,21 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return "hello world"',() =>{
+    expect(controller.get()).toBe("ini helloworld")
+  })
+
+  it('should return cookie value ',() =>{
+    
+    const mockRequest = httpMocks.createRequest({
+      cookies: {
+        name: "farhan yudha pratama"
+      }
+    })
+
+    const result = controller.GetCookie(mockRequest)
+
+    expect(result).toBe("Cookie value is farhan yudha pratama")
+  })
 });
