bc3dd6bb9f9e43c00d9f78d7a7491f45a4716011
roronoa52 <farhan.yudha2016we@gmail.com>
2025-01-21 14:14:01 +0700

    belajar http response dan route

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['**/*.ts', '**/*.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -28,8 +28,8 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off'
     },
   },
 );
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+
+    @Post()
+    post(): string{
+
+        return "ini post";
+    }
+
+    @Get("/helloworld")
+    get(): string{
+
+        return "ini helloworld";
+    }
+
+}
