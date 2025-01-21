b9f7b86559bfba690937164a16e8eab70e6b2b2b
roronoa52 <farhan.yudha2016we@gmail.com>
2025-01-21 14:32:03 +0700

    belajar http request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+
+    @Get()
+    getByQuery(
+        @Query("first_name") firstName:string,
+        @Query("last_name") lastName:string,
+    ): string{
+
+        return `Hi ${firstName} ${lastName}`;
+    }
+
+    @Get("/:id")
+    getById(@Req() request:Request): string{
+
+        return `Get user with id ${request.params.id}`;
+    }
 
     @Post()
     post(): string{
