7e053b5b78a4d17b4dca84fdbd65a601b15e4bf8
roronoa52 <farhan.yudha2016we@gmail.com>
2025-01-22 14:43:17 +0700

    global exception

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationFilter } from './validation/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.useLogger(logger);
 
   app.use(cookieParser());
+
+  app.useGlobalFilters(new ValidationFilter());
 
   const configServer = app.get(ConfigService);
   await app.listen(configServer.get("PORT") || 3000);
